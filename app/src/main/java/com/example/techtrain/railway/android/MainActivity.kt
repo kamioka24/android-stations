6a6ae2761b58cec4c008725a7424414b6e6e2010
kamioka24 <keee2404@gmail.com>
2021-09-09 12:19:04 +0900

    [Finish] android_9

--- app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
+++ app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.example.techtrain.railway.android
 
 import android.os.Bundle
-import android.widget.ImageView
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<TextView>(R.id.text).setText(R.string.hello)
-//        findViewById<ImageView>(R.id.image).setImageResource(R.color.black)
+        var textView = findViewById<TextView>(R.id.textView)
+        textView.text = "ここが変わるよ！"
+//        findViewById<ImageView>(R.id.imageView).setImageResource(R.color.black)
+
+        var editText = findViewById<EditText>(R.id.editText)
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            textView.text = editText.text.toString()
+        }
+
+        // リアルタイムで editText に入力している文字を textView に反映させる。
+//        editText.addTextChangedListener {
+//            textView.text = it.toString()
+//        }
     }
 }
