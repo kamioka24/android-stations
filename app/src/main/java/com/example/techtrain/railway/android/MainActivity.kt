cfb8d3014bdc033f1ab0ba3ab214bc6be5c2f34b
kamioka24 <keee2404@gmail.com>
2021-09-09 15:35:31 +0900

    [Finish] android_10

--- app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
+++ app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
@@ -6,21 +6,30 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
+import com.example.techtrain.railway.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // ViewBinding の必須設定 (Activity 編) ① ~ ③
+
+    // ① binding クラスを lateinit var で宣言。
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
-        var textView = findViewById<TextView>(R.id.textView)
-        textView.text = "ここが変わるよ！"
-//        findViewById<ImageView>(R.id.imageView).setImageResource(R.color.black)
+        // ② binding の初期化とついでに setContentView も行う。
+        binding= ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(this.root)
+        }
 
-        var editText = findViewById<EditText>(R.id.editText)
-        val button = findViewById<Button>(R.id.button)
+        // ③ アクセスしたい View の id は勝手にキャメルケースに代わる。
 
-        button.setOnClickListener {
-            textView.text = editText.text.toString()
+        binding.textView.text = "ここが変わるよ！"
+
+        binding.button.setOnClickListener {
+            binding.textView.text = binding.editText.text.toString()
         }
 
         // リアルタイムで editText に入力している文字を textView に反映させる。
