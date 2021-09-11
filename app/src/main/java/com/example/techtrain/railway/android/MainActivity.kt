670f33e2ac154b0a9aeb7b1275468ca9b3c4f28d
kamioka24 <keee2404@gmail.com>
2021-09-11 23:32:13 +0900

    [Finish] android_11

--- app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
+++ app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.example.techtrain.railway.android
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.addTextChangedListener
 import com.example.techtrain.railway.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,19 +14,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
 
         // ② binding の初期化とついでに setContentView も行う。
-        binding= ActivityMainBinding.inflate(layoutInflater).apply {
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
             setContentView(this.root)
         }
 
         // ③ アクセスしたい View の id は勝手にキャメルケースに代わる。
+//        binding.textView.text = "ここが変わるよ！！"
+//
+//        binding.button.setOnClickListener {
+//            binding.textView.text = binding.editText.text.toString()
+//        }
 
-        binding.textView.text = "ここが変わるよ！"
-
-        binding.button.setOnClickListener {
-            binding.textView.text = binding.editText.text.toString()
+        binding.resultButton.setOnClickListener {
+            val intent = Intent(this, ResultActivity::class.java)
+            startActivity(intent)
         }
 
         // リアルタイムで editText に入力している文字を textView に反映させる。
