a389d3da45b99643d6be1827d481122ab399a89f
kamioka24 <keee2404@gmail.com>
2021-09-12 00:27:55 +0900

    [Finish] android_13

--- app/src/main/java/com/example/techtrain/railway/android/ResultActivity.kt
+++ app/src/main/java/com/example/techtrain/railway/android/ResultActivity.kt
@@ -2,6 +2,7 @@ package com.example.techtrain.railway.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.techtrain.railway.android.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
@@ -11,6 +12,8 @@ class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        Log.d("MainActivity", "クリエイト")
+
         binding = ActivityResultBinding.inflate(layoutInflater).apply {
             setContentView(this.root)
         }
@@ -18,5 +21,30 @@ class ResultActivity : AppCompatActivity() {
         val message = intent.getStringExtra("KEY_INPUT_TEXT")
 
         binding.resultTextView.text = message
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity", "スタート")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity", "リジューム")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity", "パース")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity", "ストップ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity", "デストロイ")
     }
 }
